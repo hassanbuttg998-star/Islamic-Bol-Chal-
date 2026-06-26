cb9dac16542e73c929863df1e9cc4ade61b13ad9
hassanbuttg998-star <hassanbuttg998@gmail.com>
2026-06-26 16:21:11 +0500

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const popup = document.getElementById("languagePopup");
-
-document.querySelectorAll(".lang-btn").forEach(btn=>{
-btn.addEventListener("click",()=>{
-popup.style.display="none";
-});
-});
